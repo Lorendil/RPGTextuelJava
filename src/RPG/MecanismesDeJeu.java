43f1e3b71b0d511c49d51b5d6bf3379c211fb1e1
Quentin Gueguen <quentingueguen@yahoo.fr>
2022-12-05 15:53:04 +0100

    Ajout de certaines mécaniques

--- src/RPG/MecanismesDeJeu.java
+++ src/RPG/MecanismesDeJeu.java
@@ -1,13 +1,33 @@
 package RPG;
 
-import java.util.List;
+import java.util.Random;
 
 public class MecanismesDeJeu {
-    void combat (Personnages joueur, List<Creature> listeAdversaires){
+    void combat (Personnages joueur, Creature adversaire){
 
     }
 
-    void salles(){
+    int calculDesDegats(Personnages attaquant, Personnages defenseur){
+        int bonusAttaquant = 0;
+        int bonusDefenseur = 0;
+        Random randomizer = new Random();
+        int testCrit = 0;
+        int total;
+
+        testCrit = randomizer.nextInt(100) + 1;
+        if (attaquant.critique >= testCrit) {
+            bonusAttaquant = (attaquant.attaque / 2);
+        }
+
+        total = (attaquant.attaque + bonusAttaquant) - (defenseur.defense + bonusDefenseur);
+
+        return total;
+    }
+
+    void salle(int numeroDeLaSalle){
+        if (numeroDeLaSalle == 5 || numeroDeLaSalle == 10 || numeroDeLaSalle == 15) {
+            System.out.println("Vous gagnez un niveau, sélectionnez votre bonus");
+        }
 
     }
     void intersalles(){
