b8cfb0745dcf08c6a99dad1093ba3299b4bfe77e
Quentin Gueguen <quentingueguen@yahoo.fr>
2022-12-28 12:37:46 +0100

    Modifications mineures de test + fin de partie en cas de hp <= 0

--- src/RPG/Jeu.java
+++ src/RPG/Jeu.java
@@ -3,17 +3,26 @@ package RPG;
 public class Jeu {
 	public static void main(String[] args) {
 		int salle = 1;
+		boolean statutPartie = true;
 
 		MecanismesDeJeu partie = new MecanismesDeJeu();
 
 		partie.showMainMenu();
 		System.out.println(partie.joueur);
+		partie.joueur.heal(-100);
+		System.out.println(partie.joueur);
 
-		while (salle < 21) {
+		while (salle < 21 && statutPartie) {
 			partie.salle(salle);
 			partie.intersalles();
 			salle += 1;
+			if (partie.joueur.vitalite <= 0) {
+				System.out.println("Vous avez péri, partie terminée");
+				statutPartie = false;
+			}
 		}
-		System.out.println("Bravo, vous avez gagné la partie !");
+		if (salle == 21){
+			System.out.println("Bravo, vous avez gagné la partie !");
+		}
 	}
 }
